c0d86d3b5fa2aa4efb9149b282ccbfbd03798b71
craigchihombe <achihombe@gmail.com>
2025-01-08 15:33:55 +0200

    Add files via upload

--- Frequency check.py
+++ Frequency check.py
@@ -1,5 +1,95 @@
 test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
 print("The original dictinary is: "+ str(test_dict))
+A=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==A:
+        res = res+1
+        
+print("Frequency of A is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+B=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==B:
+        res = res+1
+        
+print("Frequency of B is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+C=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==C:
+        res = res+1
+        
+print("Frequency of C is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+D=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==D:
+        res = res+1
+        
+print("Frequency of D is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+E=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==E:
+        res = res+1
+        
+print("Frequency of E is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+F=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==F:
+        res = res+1
+        
+print("Frequency of F is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+G=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==G:
+        res = res+1
+        
+print("Frequency of G is: "+str(res)) 
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+I=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==I:
+        res = res+1
+        
+print("Frequency of H is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+J=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==J:
+        res = res+1
+        
+print("Frequency of J is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
 K=2
 res = 0
 for key in test_dict:
@@ -7,3 +97,253 @@ for key in test_dict:
         res = res+1
         
 print("Frequency of K is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+L=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==L:
+        res = res+1
+        
+print("Frequency of L is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+M=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==M:
+        res = res+1
+        
+print("Frequency of M is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+N=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==N:
+        res = res+1
+        
+print("Frequency of N is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+O=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==O:
+        res = res+1
+        
+print("Frequency of O is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+P=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==P:
+        res = res+1
+        
+print("Frequency of P is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+Q=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==Q:
+        res = res+1
+        
+print("Frequency of Q is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+R=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==R:
+        res = res+1
+        
+print("Frequency of R is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+S=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==S:
+        res = res+1
+        
+print("Frequency of S is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+T=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==T:
+        res = res+1
+        
+print("Frequency of T is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+U=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==U:
+        res = res+1
+        
+print("Frequency of U is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+V=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==V:
+        res = res+1
+        
+print("Frequency of V is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+W=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==W:
+        res = res+1
+        
+print("Frequency of W is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+X=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==X:
+        res = res+1
+        
+print("Frequency of X is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+Y=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==Y:
+        res = res+1
+        
+print("Frequency of Y is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+Z=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==Z:
+        res = res+1
+        
+print("Frequency of Z is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+1=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==1:
+        res = res+1
+        
+print("Frequency of 1 is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+2=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==2:
+        res = res+1
+        
+print("Frequency of 2 is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+3=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==3:
+        res = res+1
+        
+print("Frequency of 3 is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+4=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==4:
+        res = res+1
+        
+print("Frequency of 4 is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+5=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==5:
+        res = res+1
+        
+print("Frequency of 5 is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+6=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==6:
+        res = res+1
+        
+print("Frequency of 6 is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+7=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==7:
+        res = res+1
+        
+print("Frequency of 7 is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+8=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==8:
+        res = res+1
+        
+print("Frequency of 8 is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+9=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==9:
+        res = res+1
+        
+print("Frequency of 9 is: "+str(res))
+
+test_dict = {'Codingal':2, 'is':2, 'Best':2, 'for':2, 'Coding':1}
+print("The original dictinary is: "+ str(test_dict))
+0=2
+res = 0
+for key in test_dict:
+    if test_dict[key]==0:
+        res = res+1
+        
+print("Frequency of 0 is: "+str(res))
